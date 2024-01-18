c08ce6664a4294d32f8da9ddee185753afe8a309
Shubham Dixit <shubhamdixit863@gmail.com>
2024-01-18 23:31:52 +0530

    GitsyncProcessor With in memory repository cloning (#26)
    * added gitsyncprocessor
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    
    * cloning repo based on tag branch name and commit hash
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    
    * cloning repo based on tag branch name and commit hash
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    
    * addressed comments
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    
    * test cases fixes
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    
    * added cloning based on commit hash
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    
    * added target revision
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    
    * added target revision
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    
    * added target revision
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    
    * check revision with cloning updated
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    
    * check revision with cloning updated
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    
    * addressed comments
    
    Signed-off-by: shubham <shubhamdixit863@gmail.com>
    
    * addressed comments
    
    Signed-off-by: shubham <shubhamdixit863@gmail.com>
    
    ---------
    
    Signed-off-by: shubham dixit <shubhamdixit863@gmail.com>
    Signed-off-by: shubham <shubhamdixit863@gmail.com>

--- internal/git/gitsync_processor.go
+++ internal/git/gitsync_processor.go
@@ -1,19 +1,93 @@
 package git
 
 import (
+	"log"
+	"regexp"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/storage/memory"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	numaplanenumaprojiov1 "github.com/numaproj-labs/numaplane/api/v1"
+	v1 "github.com/numaproj-labs/numaplane/api/v1"
 )
 
+const messageChanLength = 5
+
+type Message struct {
+	Updates bool
+	Err     error
+}
+
+func isCommitSHA(revision string) bool {
+	match, _ := regexp.MatchString("^[0-9a-fA-F]{40}$", revision)
+	return match
+}
+
 type GitSyncProcessor struct {
+	gitSync     v1.GitSync
+	channels    map[string]chan Message
+	k8Client    client.Client
+	clusterName string
 }
 
-func NewGitSyncProcessor(gitSync *numaplanenumaprojiov1.GitSync, k8sClient client.Client) (*GitSyncProcessor, error) {
-	return &GitSyncProcessor{}, nil
+func cloneRepo(repoPath *v1.RepositoryPath) {
+	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL: repoPath.RepoUrl,
+	})
+	if err != nil {
+		log.Fatalf("error cloning the repository %s", err.Error())
+	}
+	// TargetRevision can be a branch, a tag, or a commit hash
+	err = checkRevision(r, repoPath.TargetRevision)
+	if err != nil {
+		log.Fatalf("error  checking revision for the repository %s", err.Error())
+
+	}
 }
 
-func (processor *GitSyncProcessor) Update(gitSync *numaplanenumaprojiov1.GitSync) error {
+func checkRevision(r *git.Repository, revision string) error {
+	hash, err := r.ResolveRevision(plumbing.Revision(revision))
+	if err != nil {
+		return err
+	}
+	w, err := r.Worktree()
+	if err != nil {
+		return err
+	}
+
+	err = w.Checkout(&git.CheckoutOptions{
+		Hash: *hash,
+	})
+	if err != nil {
+		return err
+	}
+
+	if isCommitSHA(revision) {
+		log.Println("No monitoring")
+	} else {
+		log.Println("monitoring")
+	}
+	return nil
+}
+
+func NewGitSyncProcessor(gitSync *v1.GitSync, k8client client.Client, clusterName string) (*GitSyncProcessor, error) {
+	channels := make(map[string]chan Message)
+	for _, repo := range gitSync.Spec.RepositoryPaths {
+		gitCh := make(chan Message, messageChanLength)
+		channels[repo.Name] = gitCh
+		go cloneRepo(&repo)
+	}
+	return &GitSyncProcessor{
+		gitSync:     *gitSync,
+		k8Client:    k8client,
+		channels:    channels,
+		clusterName: clusterName,
+	}, nil
+
+}
+
+func (processor *GitSyncProcessor) Update(gitSync *v1.GitSync) error {
 	return nil
 }
 
