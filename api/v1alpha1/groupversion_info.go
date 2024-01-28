5e8b198b8e85686cb43d5fb1bef413ce8d2e7563
Hao Hao <xdevxhao@gmail.com>
2024-01-27 23:50:46 -0800

    Update API group and version
    Signed-off-by: Hao Hao <xdevxhao@gmail.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -14,10 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1 contains API Schema definitions for the numaplane.numaproj.io v1 API group
+// Package v1alpha1 contains API Schema definitions for the numaplane.numaproj.io v1alpha1 API group
 // +kubebuilder:object:generate=true
-// +groupName=numaplane.numaproj.io.github.com.numaproj-labs
-package v1
+// +groupName=numaplane.numaproj.io
+package v1alpha1
 
 import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "numaplane.numaproj.io.github.com.numaproj-labs", Version: "v1"}
+	GroupVersion = schema.GroupVersion{Group: "numaplane.numaproj.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
