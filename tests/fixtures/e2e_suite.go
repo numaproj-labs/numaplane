9c706ebe12cc92e1384f49ef8f8c13cd1a8800a0
Dillen Padhiar <dillen_padhiar@intuit.com>
2024-03-14 14:25:38 -0700

    feat: create E2ESuite struct with start/teardown funcs
    Signed-off-by: Dillen Padhiar <dillen_padhiar@intuit.com>

--- tests/fixtures/e2e_suite.go
+++ tests/fixtures/e2e_suite.go
@@ -14,4 +14,114 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package fixture
+package fixtures
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	/* resource names */
+	Namespace      = "numaplane-system"
+	Label          = "numaplane-e2e"
+	defaultTimeout = 60 * time.Second
+)
+
+var (
+	background = metav1.DeletePropagationBackground
+)
+
+type E2ESuite struct {
+	suite.Suite
+	restConfig *rest.Config
+	kubeClient kubernetes.Interface
+	stopch     chan struct{}
+}
+
+func (s *E2ESuite) SetupSuite() {
+	var err error
+	s.restConfig, err = nil, nil
+	s.CheckError(err)
+	s.kubeClient, err = kubernetes.NewForConfig(s.restConfig)
+	s.CheckError(err)
+
+	// resource cleanup
+	s.deleteResources([]schema.GroupVersionResource{
+		metav1.SchemeGroupVersion.WithResource("gitsync"),
+	})
+
+	/* portfwd git server pod */
+	// err = PodPortForward(s.restConfig, Namespace, "git-server-pod", 8443, 8443,s.stopch)
+	// s.CheckError(err)
+
+}
+
+func (s *E2ESuite) TearDownSuite() {
+	s.deleteResources([]schema.GroupVersionResource{
+		metav1.SchemeGroupVersion.WithResource("gitsync"),
+	})
+	close(s.stopch)
+}
+
+func (s *E2ESuite) CheckError(err error) {
+	s.T().Helper()
+	if err != nil {
+		s.T().Fatal(err)
+	}
+}
+
+func (s *E2ESuite) deleteResources(resources []schema.GroupVersionResource) {
+	hasTestLabel := metav1.ListOptions{LabelSelector: Label}
+	ctx := context.Background()
+	for _, r := range resources {
+		err := s.dynamicFor(r).DeleteCollection(ctx, metav1.DeleteOptions{PropagationPolicy: &background}, hasTestLabel)
+		s.CheckError(err)
+	}
+
+	for _, r := range resources {
+		for {
+			list, err := s.dynamicFor(r).List(ctx, hasTestLabel)
+			s.CheckError(err)
+			if len(list.Items) == 0 {
+				break
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+func (s *E2ESuite) dynamicFor(r schema.GroupVersionResource) dynamic.ResourceInterface {
+	resourceInterface := dynamic.NewForConfigOrDie(s.restConfig).Resource(r)
+	return resourceInterface.Namespace(Namespace)
+}
+
+// k8sRestConfig returns a rest config for the kubernetes cluster.
+// TODO: move to different file if needs to be shared
+func k8sRestConfig() (*rest.Config, error) {
+	var restConfig *rest.Config
+	var err error
+	kubeconfig := os.Getenv("KUBECONFIG")
+	if kubeconfig == "" {
+		home, _ := os.UserHomeDir()
+		kubeconfig = home + "/.kube/config"
+		if _, err := os.Stat(kubeconfig); err != nil && os.IsNotExist(err) {
+			kubeconfig = ""
+		}
+	}
+	if kubeconfig != "" {
+		restConfig, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+	} else {
+		restConfig, err = rest.InClusterConfig()
+	}
+	return restConfig, err
+}
