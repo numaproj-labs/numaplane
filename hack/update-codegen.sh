a112f53927319c734dcc24cdba8d21144b698bff
Hao Hao <xdevxhao@gmail.com>
2024-05-20 11:29:06 -0700

    use kube_codegen.sh instead in codegen
    Signed-off-by: Hao Hao <xdevxhao@gmail.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -17,7 +17,7 @@ cd "${FAKE_REPOPATH}"
 
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${FAKE_REPOPATH}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
-bash -x ${CODEGEN_PKG}/generate-groups.sh "client" \
+bash -x ${CODEGEN_PKG}/kube_codegen.sh "client" \
   github.com/numaproj-labs/numaplane/pkg/client github.com/numaproj-labs/numaplane/pkg/apis \
   "numaplane:v1alpha1" \
   --go-header-file hack/boilerplate.go.txt
