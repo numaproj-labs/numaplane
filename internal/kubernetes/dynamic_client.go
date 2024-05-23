ac812f8aab128a4df280e8eca92936e9a699b7c3
Julie Vogelman <julievogelman0@gmail.com>
2024-05-22 21:28:31 -0700

    propagate status
    Signed-off-by: Julie Vogelman <julievogelman0@gmail.com>

--- internal/kubernetes/dynamic_client.go
+++ internal/kubernetes/dynamic_client.go
@@ -155,8 +155,6 @@ func objectToUnstructured(object *GenericObject) (*unstructured.Unstructured, er
 	return &unstructured.Unstructured{Object: asMap}, nil
 }
 
-// unused - will keep and comment out in case it's needed later
-
 func unstructuredToObject(u *unstructured.Unstructured) (*GenericObject, error) {
 	asJsonBytes, err := json.Marshal(u.Object)
 	if err != nil {
